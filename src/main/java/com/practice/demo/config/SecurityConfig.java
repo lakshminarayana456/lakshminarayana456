70fd53e4d6e678aec0e51cbc12ab321bc2126148
lakshminarayana456 <lakshmanlucky4560@gmail.com>
2024-10-04 19:18:43 +0530

    feature1 change3

--- src/main/java/com/practice/demo/config/SecurityConfig.java
+++ src/main/java/com/practice/demo/config/SecurityConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -33,7 +31,6 @@ public class SecurityConfig {
         });
         return http.build();
     }
-
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
